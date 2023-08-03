8d6933bed08fd26563ee3a30a9a1ffd92ffcf0f2
Manutsawee <manutsawee.louis@outlook.com>
2023-08-03 23:54:26 +0800

    Optimize choose world button

--- modservercreationmain.lua
+++ modservercreationmain.lua
@@ -8,13 +8,13 @@ modimport("main/toolutil")
 modimport("main/strings")
 modimport("modfrontendmain")
 
-local Menu = require("widgets/menu")
-local ImageButton = require("widgets/imagebutton")
-local PopupDialogScreen = require("screens/redux/popupdialog")
+local TEMPLATES = require "widgets/redux/templates"
+local PopupDialogScreen = require "screens/redux/popupdialog"
+local ChooseWorldSreen = require "widgets/redux/chooseworldscreen"
 
 IACore.WorldLocations = {
-    [1] = {FOREST = true, CAVE = true},
-    [2] = {CAVE = true},
+    [1] = {CAVE = true, FOREST = true},
+    [2] = {CAVE = true}
 }
 
 function IACore.SetLevelLocations(servercreationscreen, location, i)
@@ -26,16 +26,19 @@ function IACore.SetLevelLocations(servercreationscreen, location, i)
     servercreationscreen.world_config_tabs.menu.items[i + 1]:SetText(text)
 end
 
-local function ChooseWorld(i, world_tab, servercreationscreen)
-    local menuitems = {}
-    for location, val in pairs(IACore.WorldLocations[i]) do
-        table.insert(menuitems, {text = STRINGS.UI.SANDBOXMENU.LOCATIONTABNAME[location], cb = function()
-            IACore.SetLevelLocations(servercreationscreen, location:lower(), i)
-            TheFrontEnd:PopScreen()
-        end, style = "carny_long"})
+local function OnWorldButton(world_tab, i)
+    if world_tab.settings_widget:IsNewShard() then
+        if world_tab:GetParentScreen() then
+            world_tab:GetParentScreen().last_focus = TheFrontEnd:GetFocusWidget()
+        end
+        local currentworld = world_tab:GetLocation()
+        local chooseworldscreen = ChooseWorldSreen(world_tab, currentworld, i)
+        TheFrontEnd:PushScreen(chooseworldscreen)
+    else
+        local cancel_button = {{text = STRINGS.UI.LOBBYSCREEN.CANCEL, cb = function() TheFrontEnd:PopScreen() end, style = "carny_long"}}
+        local popupdialogscreen = PopupDialogScreen(STRINGS.UI.SANDBOXMENU.CHOSENWORLD, STRINGS.UI.CUSTOMIZATIONSCREEN.CHOSENWORLD, cancel_button)
+        TheFrontEnd:PushScreen(popupdialogscreen)
     end
-    table.insert(menuitems, {text = STRINGS.UI.LOBBYSCREEN.CANCEL, cb = function() TheFrontEnd:PopScreen() end, style = "carny_long"})
-    TheFrontEnd:PushScreen(PopupDialogScreen(STRINGS.UI.SANDBOXMENU.CHOOSEWORLD, STRINGS.UI.CUSTOMIZATIONSCREEN.CUSTOMDESC, menuitems))
 end
 
 scheduler:ExecuteInTime(0, function()  -- Delay a frame so we can get ServerCreationScreen when entering a existing world
@@ -46,26 +49,18 @@ scheduler:ExecuteInTime(0, function()  -- Delay a frame so we can get ServerCrea
     end
 
     for i, world_tab in ipairs(servercreationscreen.world_tabs) do
-        for i, world_tab in ipairs(servercreationscreen.world_tabs) do
-            if servercreationscreen:CanResume() and world_tab:GetLocation() ~= SERVER_LEVEL_LOCATIONS[i] then
-                SERVER_LEVEL_LOCATIONS[i] = world_tab:GetLocation()
-                servercreationscreen.world_tabs[i]:RefreshOptionItems()
-                local text = servercreationscreen.world_tabs[i]:GetLocationTabName()
-                servercreationscreen.world_config_tabs.menu.items[i + 1]:SetText(text)
-            end
+        if servercreationscreen:CanResume() and world_tab:GetLocation() ~= SERVER_LEVEL_LOCATIONS[i] then
+            SERVER_LEVEL_LOCATIONS[i] = world_tab:GetLocation()
+            servercreationscreen.world_tabs[i]:RefreshOptionItems()
+            local text = servercreationscreen.world_tabs[i]:GetLocationTabName()
+            servercreationscreen.world_config_tabs.menu.items[i + 1]:SetText(text)
         end
 
         if not world_tab.choose_world_button then
-            world_tab.choose_world_button = world_tab.settings_root:AddChild(ImageButton("images/global_redux.xml", "button_carny_long_normal.tex", "button_carny_long_hover.tex", "button_carny_long_disabled.tex", "button_carny_long_down.tex"))
-
+            world_tab.choose_world_button = world_tab.settings_root:AddChild(TEMPLATES.StandardButton(function() OnWorldButton(world_tab, i) end, STRINGS.UI.SANDBOXMENU.CHOOSEWORLD))
             world_tab.choose_world_button.image:SetScale(.47)
-            world_tab.choose_world_button:SetFont(CHATFONT)
             world_tab.choose_world_button.text:SetColour(0, 0, 0, 1)
-            world_tab.choose_world_button:SetOnClick(function(self, ...)
-                ChooseWorld(i, world_tab, servercreationscreen)
-            end)
             world_tab.choose_world_button:SetTextSize(19.6)
-            world_tab.choose_world_button:SetText(STRINGS.UI.SANDBOXMENU.CHOOSEWORLD)
             world_tab.choose_world_button:SetPosition(320, 285)
         elseif not world_tab.choose_world_button.shown then
             world_tab.choose_world_button:Show()
--- strings/common.lua
+++ strings/common.lua
@@ -1,7 +1,17 @@
+local STRINGS = GLOBAL.STRINGS
+
 return {
     UI = {
         SANDBOXMENU = {
             CHOOSEWORLD = "Choose World",
+            CHOSENWORLD = "You have chosen the world!",
+        },
+        CUSTOMIZATIONSCREEN = {
+            CHOSENWORLD = "Can't choose again",
+            PRESETLEVELDESC = {
+                FOREST = STRINGS.UI.CUSTOMIZATIONSCREEN.PRESETLEVELDESC.SURVIVAL_TOGETHER,
+                CAVE = STRINGS.UI.CUSTOMIZATIONSCREEN.PRESETLEVELDESC.DST_CAVE,
+            }
         },
     },
 }
