1ee6e1351b1c76098e1f932f8fdc8ece8f0d733a
Jerry <2234834467@qq.com>
2023-08-15 14:07:36 +0800

    machete correct use water anim

--- scripts/prefabs/machete.lua
+++ scripts/prefabs/machete.lua
@@ -44,7 +44,7 @@ local function pristinefn()
     inst:AddTag("weapon")
 
     MakeInventoryFloatable(inst)
-    -- inst.components.floater:UpdateAnimations("idle_water", "idle")
+    inst.components.floater:UpdateAnimations("idle_water", "idle")
 
     return inst
 end
